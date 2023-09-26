335d39ea9b84f44ff7adeff28e590af2fe20f21d
LuiGeeDev <yjs3475@gmail.com>
2023-09-26 15:25:02 +0900

    Exercise 1

---
+++ src/components/GuessInput/GuessInput.js
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+
+function GuessInput() {
+  const [word, setWord] = useState("");
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log(word);
+    setWord("");
+  }
+
+  return (
+    <form class="guess-input-wrapper" onSubmit={handleSubmit}>
+      <label htmlFor="guess-input">Enter guess:</label>
+      <input
+        id="guess-input"
+        type="text"
+        minLength={5}
+        maxLength={5}
+        pattern="[a-zA-Z]{5}"
+        value={word}
+        onChange={(e) => setWord(e.target.value.toUpperCase())}
+        title="Must be 5 characters"
+      />
+    </form>
+  );
+}
+
+export default GuessInput;
