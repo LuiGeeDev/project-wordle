42532fb4d960d8f43df02702f5db75a7b0e086b8
LuiGeeDev <yjs3475@gmail.com>
2023-09-26 15:43:58 +0900

    constants

--- src/components/UserGuesses/UserGuesses.js
+++ src/components/UserGuesses/UserGuesses.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { range } from "../../utils";
 import UserGuess from "../UserGuess/UserGuess";
+import { NUM_OF_GUESSES_ALLOWED } from "../../constants";
 
 function UserGuesses({ guesses = [] }) {
   return (
     <div className="guess-results">
-      {range(6).map((num) => (
+      {range(NUM_OF_GUESSES_ALLOWED).map((num) => (
         <UserGuess guess={guesses[num]} key={num} />
       ))}
     </div>
