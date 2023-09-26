a4b6ce5c076c8b7e33a4fd9d89d41b7981aae32e
LuiGeeDev <yjs3475@gmail.com>
2023-09-26 15:26:23 +0900

    Exercise 1 Fixed

--- src/components/GuessInput/GuessInput.js
+++ src/components/GuessInput/GuessInput.js
@@ -10,11 +10,12 @@ function GuessInput() {
   }
 
   return (
-    <form class="guess-input-wrapper" onSubmit={handleSubmit}>
+    <form className="guess-input-wrapper" onSubmit={handleSubmit}>
       <label htmlFor="guess-input">Enter guess:</label>
       <input
         id="guess-input"
         type="text"
+        required
         minLength={5}
         maxLength={5}
         pattern="[a-zA-Z]{5}"
